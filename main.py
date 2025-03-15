998addc813d2a1d37fb9fa158141e97b369a986b
argarm <aarongrmrr@gmail.com>
2025-03-15 18:27:21 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+﻿from fastapi import FastAPI
+
+app = FastAPI()
+@app.get("/games")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/games/{game_id}")
+async def say_hello(game_id: int ):
+    return {"message": f"Hello {game_id}!"}
